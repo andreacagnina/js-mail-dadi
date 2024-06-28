18224bb3db3ee39fa87e07315c9bba207eea1543
andreacagnina <amirkal@libero.it>
2024-06-28 15:18:25 +0200

    [feat] created array

---
+++ mail/js/script.js
@@ -0,0 +1,8 @@
+// creo un array di mail con le autorizzazioni per accedere
+
+let vip = ["pippo@libero.it", "paperino@live.it", "pluto@hotmail.com", "topolino@virgilio.it", "nonnapapera@gmail.com"];
+
+// faccio un console log del vip per vedere che tutto funzioni correttamente
+console.log(vip);
+// faccio un console log all'interno dell'array per vedere che tutto funzioni correttamente
+console.log(vip[0]);
